50135da592b611d83d916ec47ee1d7521afea0b6
Felipe Ribeiro <felipedejesusifgoiano@gmail.com>
2022-09-15 07:49:19 -0300

    modified:   src/App.js 	new file:   src/components/Item.js 	new file:   src/components/List.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyname from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import List from './components/List';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
       idade="31" 
       profissao="Programador" 
       foto="https://via.placeholder.com/150"/>
+      <List/>
     </div>
   );
 }
